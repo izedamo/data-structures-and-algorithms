4308126cc202ca0dd47e878cde865b7ae0a5e3b2
Shubham Bassi <Shubham.Bassi@wolterskluwer.com>
2022-02-02 23:15:51 +0530

    added HaveCommonItems with O(m+n) time complexity.

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Exercises.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    class Exercises
+    {
+        //O(m+n) = Linear time.
+        public static bool HaveCommonItems(string[] firstArray, string[] secondArray)
+        {
+            var hashset = new HashSet<string>(firstArray.Length); //O(1) lookup
+
+            foreach(var elem in firstArray) //O(n)
+            {
+                hashset.Add(elem);
+            }
+
+            foreach(var elem in secondArray) //O(m)
+            {
+                if (hashset.Contains(elem))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Exercises.HaveCommonItems(new string[] {  }, new string[] {  });
+            Console.WriteLine(result);
+        }
+    }
+}
